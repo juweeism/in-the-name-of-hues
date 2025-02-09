953fd023cab84a500dfde4a4e2b7cb2d4f3292a5
juweeism <mijane.404@gmail.com>
2025-02-09 22:21:26 +0800

    fix rss content parsing

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
     const digest = await getCollection('digest')
@@ -12,7 +15,9 @@ export async function GET(context) {
             title: d.data.title,
             pubDate: d.data.pubDate,
             tags: d.data.tags,
-            content: d.body
+            content: sanitizeHtml(parser.render(d.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
         })),
         customData: `<language>en-us</language>`,
     });
