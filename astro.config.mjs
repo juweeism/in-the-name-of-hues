f74e349e178b5276e03aec0e827af38bb44a59fd
juweeism <mijane.404@gmail.com>
2025-02-09 20:03:24 +0800

    delete react and alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
 import cloudflare from '@astrojs/cloudflare';
 
 import tailwind from '@astrojs/tailwind';
 
-import alpinejs from '@astrojs/alpinejs';
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), alpinejs()],
+    integrations: [tailwind()],
     adapter: cloudflare(),
     site: import.meta.env.DEV ? 'http://localhost:4321' : 'https://digest.juw.ee'
 });
